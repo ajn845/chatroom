522b29c7841808f0e7f6fa9b3ede46d72e36b59b
ajn845 <ajn845@gmail.com>
2020-06-03 12:01:00 -0700

    new commit. not final

---
+++ client/src/components/Join.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+
+const Join = () => {
+
+    const [name, setName] = useState('');
+    const [romm, setRame] = useState('');
+
+    
+ return(
+    <h1>Join</h1>
+    )
+}
+
+
+export default Join;
